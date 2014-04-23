f43147caf6dab74d9fea888c12ecb8b5da16f2f9
Collin Reeser <collin.reeser@gmail.com>
2014-04-23 01:00:05 -0400

    Updated examples makefile with realclean, updated timing for primes

--- examples/primesCall.c
+++ examples/primesCall.c
@@ -4,13 +4,11 @@ extern void primes(int start);
 
 void main()
 {
-    // primes(50);
-
-    primes(250000);
+    primes(50);
 
     // time ./primes > primesOutput
-    // real    0m15.390s
-    // user    0m15.377s
+    // real    0m2.095s
+    // user    0m2.087s
     // sys     0m0.007s
     // primes(250000);
 }
